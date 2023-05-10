c0855404fda66ef4282e9756baad578f953a654a
getahsaan <getahsaan@gamil.com>
2023-05-10 08:12:33 +0600

    chocolate added to the server method done

--- src/components/NewChocolates.jsx
+++ src/components/NewChocolates.jsx
@@ -2,6 +2,28 @@ import React from "react";
 import { FaLongArrowAltLeft } from "react-icons/fa";
 
 const NewChocolates = () => {
+  const handleNewChocolate = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const name = form.name.value;
+    const country = form.country.value;
+    const category = form.select.value;
+    const newChocolate = { name, country, category };
+    console.log(newChocolate);
+
+    fetch("http://localhost:4000/chocolates", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(newChocolate),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      });
+  };
+
   return (
     <div className="container m-4 p-4">
       <h1 className="text-4xl font-bold bg-amber-950 text-white p-2 w-1/2 mx-auto rounded-lg text-center">
@@ -19,7 +41,7 @@ const NewChocolates = () => {
           <p className="text-black text-center">
             Use the below form to create a new product
           </p>
-          <form>
+          <form onSubmit={handleNewChocolate}>
             <div className="my-4">
               <label
                 htmlFor="name"
@@ -37,14 +59,14 @@ const NewChocolates = () => {
             </div>
             <div className="my-4">
               <label
-                htmlFor="name"
+                htmlFor="country"
                 className="text-black text-xl m-1"
               >
                 Country
               </label>
               <input
                 type="text"
-                name="name"
+                name="country"
                 id=""
                 placeholder="Enter Country Name"
                 className="bg-white rounded-lg m-1 w-full p-2"
@@ -52,32 +74,27 @@ const NewChocolates = () => {
             </div>
             <div className="my-4">
               <label
-                htmlFor="name"
+                htmlFor="category"
                 className="text-black text-xl m-1"
               >
                 Category
               </label>
-              {/* <input
-                type="text"
-                name="name"
-                id=""
-                placeholder="Enter Country Name"
-                className="bg-white rounded-lg m-1 w-full p-2"
-              /> */}
               <div className="m-2">
-                <select className="select w-full bg-white">
-                  <option
-                    disabled
-                    selected
-                  >
-                    Premium
-                  </option>
+                <select
+                  className="select w-full bg-white"
+                  id="select"
+                >
+                  <option>Premium</option>
                   <option>Category 1</option>
                   <option>Category 2</option>
                 </select>
               </div>
             </div>
-            <button className="btn bg-amber-700 w-full hover:btn-primary my-4 text-white">Save</button>
+            <input
+              type="submit"
+              value="Save"
+              className="btn bg-amber-700 w-full hover:btn-primary my-4 text-white"
+            />
           </form>
         </div>
       </div>
